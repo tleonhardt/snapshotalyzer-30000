6d1a8059a8ef69d69e7833db9715c05dec1d33c2
Todd Leonhardt <todd.leonhardt@gmail.com>
2019-10-03 00:56:36 -0400

    Extracted list_instances to a function

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
-"""Manage EC2 instance snapshots."""
+"""Demo project to manage AWS EC2 instance snapshots using boto3."""
 import boto3
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+
+def list_instances() -> None:
+    """List running EC2 instances."""
+    for i in ec2.instances.all():
+        print(i)
 
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
-    for i in ec2.instances.all():
-        print(i)
+    list_instances()
